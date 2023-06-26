8b1d877a2e4367f84ad951007936f78b6cd937b7
Jeroen Verbeek <>
2023-06-26 14:18:36 -0700

    move initializing the SDK to app main file.

--- ios/SwiftUI/IntegrationDemo_SwiftUI/IntegrationDemo_SwiftUIApp.swift
+++ ios/SwiftUI/IntegrationDemo_SwiftUI/IntegrationDemo_SwiftUIApp.swift
@@ -6,12 +6,53 @@
 //
 
 import SwiftUI
+import TapResearchSDK
 
 @main
 struct swiftui_example_tapresearchApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+
+	let apiKey = "0b5dcbae8151c1b82d69697dce004bf2"
+	var userIdInput = "public-demo-test-user"
+	var tapSDKDelegates: TapResearchSDKDelegates! = TapResearchSDKDelegates()
+
+	///---------------------------------------------------------------------------------------------
+	init() {
+
+		tapSDKDelegates = TapResearchSDKDelegates()
+		TapResearchSDK.initialize(withAPIToken: apiKey, userIdentifier: userIdInput, sdkDelegate: tapSDKDelegates) { (error: TRError?) in
+			if ((error) != nil) {
+				print("Error \(String(describing: error))")
+			}
+		}
+	}
+
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
+
+///---------------------------------------------------------------------------------------------
+///---------------------------------------------------------------------------------------------
+class TapResearchSDKDelegates : TapResearchSDKDelegate {
+
+	///---------------------------------------------------------------------------------------------
+	func onTapResearchDidError(_ error: TRError) {
+		print("(TRError) \(#function): \(error.code), \(error.localizedDescription)")
+	}
+
+	///---------------------------------------------------------------------------------------------
+	func onTapResearchDidReceiveRewards(_ rewards: [TRReward]) {
+		print("(TRError) \(#function): \(rewards.count) rewards awarded")
+
+		var i: Int = 0
+		for reward in rewards {
+			print("reward \(i): currency = \(reward.currencyName ?? "unknown"), amount = \(reward.rewardAmount)")
+			i += 1
+		}
+	}
+
+}
+
+
