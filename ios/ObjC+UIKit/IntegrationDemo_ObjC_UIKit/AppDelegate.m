dfa3923cb3072463cf8fca003ffdad32268ebb09
Jeroen Verbeek <jeroen@tapresearch.com>
2025-09-05 15:39:02 -0700

    Add local notifications example code to ObjC+UIKit example
    Signed-off-by: Jeroen Verbeek <jeroen@tapresearch.com>

--- ios/ObjC+UIKit/IntegrationDemo_ObjC_UIKit/AppDelegate.m
+++ ios/ObjC+UIKit/IntegrationDemo_ObjC_UIKit/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TapResearchSDK/TapResearchSDK.h"
 
 @implementation AppDelegate
 
@@ -18,6 +19,8 @@
 - (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
 	// Called when a new scene session is being created.
 	// Use this method to select a configuration to create the new scene with.
+	UNUserNotificationCenter *userNotificationCenter = [UNUserNotificationCenter currentNotificationCenter];
+	[userNotificationCenter setDelegate: self];
 	return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
 }
 
@@ -25,6 +28,34 @@
 	// Called when the user discards a scene session.
 	// If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
 	// Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+		 withCompletionHandler:(void (^)(void))completionHandler
+{
+	BOOL handled = [TapResearch handleNotificationWithDidReceive:response contentDelegate:nil];
+	if (handled) {
+		// ...
+	}
+	completionHandler();
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+	   willPresentNotification:(UNNotification *)notification
+		 withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+{
+	if (![TapResearch handleNotificationPresentation:notification withCompletionHandler:completionHandler]) {
+
+		if (@available(iOS 14.0, *)) {
+			completionHandler(UNNotificationPresentationOptionSound |
+							  UNNotificationPresentationOptionBanner |
+							  UNNotificationPresentationOptionList);
+		} else {
+			completionHandler(UNNotificationPresentationOptionSound |
+							  UNNotificationPresentationOptionAlert);
+		}
+	}
 }
 
 @end
