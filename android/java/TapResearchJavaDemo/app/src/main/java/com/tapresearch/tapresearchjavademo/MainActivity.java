4953c42f983bc263e99b07a89a58b819fdaf0cd5
kkawai <kkawai2@gmail.com>
2025-11-20 19:39:27 -0800

    added grant boost sampler to java demo

--- android/java/TapResearchJavaDemo/app/src/main/java/com/tapresearch/tapresearchjavademo/MainActivity.java
+++ android/java/TapResearchJavaDemo/app/src/main/java/com/tapresearch/tapresearchjavademo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.tapresearch.tapresearchjavademo;
 
 import android.annotation.SuppressLint;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,6 +23,11 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.List;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.Toast;
+
 import kotlinx.serialization.InternalSerializationApi;
 
 @InternalSerializationApi
@@ -27,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     // Tag used for logging. Helps in identifying the source of log messages.
     private static final String LOG_TAG = "MainJavaDemo";
     private TextView statusView;
+    private Button grantBoostButton;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -46,11 +55,15 @@ public class MainActivity extends AppCompatActivity {
         userAttributes.put("age", 25);
         userAttributes.put("VIP", true);
         userAttributes.put("name", "John Doe");
-        userAttributes.put("first_seen", Instant.now().toString());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            userAttributes.put("first_seen", Instant.now().toString());
+        }
 
         // Setting the main content view layout.
         setContentView(R.layout.activity_main);
         statusView = findViewById(R.id.statusView);
+        grantBoostButton = findViewById(R.id.grantBoostButton);
+
         ListView listView = findViewById(R.id.listView);
         // Placeholder data until SDK is ready.
         String[] placements = {"Waiting for SDK to be ready..."};
@@ -109,7 +122,9 @@ public class MainActivity extends AppCompatActivity {
                 customParameters.put("age", 25);
                 customParameters.put("VIP", "true");
                 customParameters.put("name", "John Doe");
-                customParameters.put("first_seen", Instant.now().toString());
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    customParameters.put("first_seen", Instant.now().toString());
+                }
                 // Showing content for the selected placement with custom parameters.
                 TapResearch.INSTANCE.showContentForPlacement(
                         selectedItem,
@@ -132,6 +147,7 @@ public class MainActivity extends AppCompatActivity {
             // Feedback toast for item click.
             statusView.setText("Clicked: " + selectedItem);
         });
+        grantBoostButton.setVisibility(View.VISIBLE);
     }
 
     // Method to display an error toast with the provided error description.
@@ -158,6 +174,33 @@ public class MainActivity extends AppCompatActivity {
         // Displaying the total rewards earned in a toast message.
 
         statusView.setText("Congrats! You've earned [" + rewardCount + "] [" + currencyName + "]. Event type is " + eventType);
+    }
+
+    public void onGrantBoostButtonClicked(View view) {
+        // 'boost-3x-1d' is an example boost tag
+        TapResearch.INSTANCE.grantBoost("boost-3x-1d", grantBoostResponse -> {
+            if (grantBoostResponse.getSuccess() == true) {
+
+                // example placement tag 'earn-center' should now be boosted
+                TRPlacementDetails placementDetails = TapResearch.INSTANCE.getPlacementDetails("earn-center", null);
+                if (placementDetails != null) {
+                    // 'earn-center' placement details should now be boosted
+                    Log.d(LOG_TAG, "placement details: " + placementDetails);
+                    showAlertDialog(MainActivity.this, "Placement Details", placementDetails.toString());
+                }
+            } else {
+                Toast.makeText(MainActivity.this, grantBoostResponse.getError().getDescription(), Toast.LENGTH_SHORT).show();
+                Log.d(LOG_TAG, "grantBoost error: " + grantBoostResponse.getError().getDescription());
+            }
+        });
+    }
+
+    static void showAlertDialog(Context context, String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 }
