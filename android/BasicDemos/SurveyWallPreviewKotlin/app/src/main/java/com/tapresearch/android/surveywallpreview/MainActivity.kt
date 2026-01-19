5940456206200372ab73219cc7294892f366abae
kkawai <kkawai2@gmail.com>
2026-01-18 20:34:20 -0800

    insets

--- android/BasicDemos/SurveyWallPreviewKotlin/app/src/main/java/com/tapresearch/android/surveywallpreview/MainActivity.kt
+++ android/BasicDemos/SurveyWallPreviewKotlin/app/src/main/java/com/tapresearch/android/surveywallpreview/MainActivity.kt
@@ -12,11 +12,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -83,7 +86,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SurveyWallPreviewTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -115,7 +117,8 @@ class MainActivity : ComponentActivity() {
         Column(
             modifier = modifier
                 .fillMaxSize()
-                .padding(16.dp),
+                .padding(16.dp)
+                .windowInsetsPadding(WindowInsets.safeDrawing),
             verticalArrangement = Arrangement.Center
         ) {
 
